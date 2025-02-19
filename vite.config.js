77ea171cfc9331423b80aa36f3bac2ad921f321b
Raul Jimenez Ortega <rjimenez@esri.com>
2025-02-19 20:05:58 +0100

    Fixing path to local assets

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',
+  build: {
+      outDir: 'dist'
+  },
+  publicDir: 'public',
+  //base: '/dist/' // build local
+  base: '/communities-directory/dist/' // build at gh-pages
 })
+
