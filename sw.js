8ff5789aab98976ba77f1556e7faa71f56f71845
hhkaos <hhkaos@users.noreply.github.com>
2026-07-31 14:32:01 +0000

    deploy: 6532d7b17c26b262ebbcdef4121de0e7d77d6027

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "community-builders-shell-1785494606687";
+const CACHE_NAME = "community-builders-shell-1785508319675";
 const APP_CACHE_PREFIX = "community-builders-shell-";
 const APP_SHELL = ["./", "./manifest.webmanifest"];
 const DATA_FILE_PATTERN = /\/data\/.+\.(json|geojson)$/;
